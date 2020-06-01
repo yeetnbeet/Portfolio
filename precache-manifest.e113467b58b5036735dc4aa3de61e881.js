b132c090ff81ab0ef1501b7a0d2433adcc50bdc0
unknown <segalcsam@gmail.com>
2020-05-31 17:16:12 -0700

    Updates

---
+++ precache-manifest.e113467b58b5036735dc4aa3de61e881.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621295399a36d58540621220fa708a1a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "66d11b91fbc9891db1f6",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b320b37a370356acda17",
+    "url": "/Portfolio/static/js/2.4f672cb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.4f672cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d11b91fbc9891db1f6",
+    "url": "/Portfolio/static/js/main.127b1b08.chunk.js"
+  },
+  {
+    "revision": "53115eedee3b21907746",
+    "url": "/Portfolio/static/js/runtime-main.86ecc4b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
